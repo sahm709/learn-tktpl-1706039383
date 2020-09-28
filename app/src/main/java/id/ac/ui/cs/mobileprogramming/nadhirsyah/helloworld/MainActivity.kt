15c5354f2e408cba62c6bf493e12d344bb32aa6e
Nadhirsyah <nadhirsyah@gmail.com>
2020-09-28 19:30:54 +0700

    add lab-2

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nadhirsyah/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nadhirsyah/helloworld/MainActivity.kt
@@ -1,12 +1,59 @@
 package id.ac.ui.cs.mobileprogramming.nadhirsyah.helloworld
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var isNightModeOn : Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val appSettingPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
+        val sharedPrefsEdit: SharedPreferences.Editor = appSettingPrefs.edit()
+        isNightModeOn = appSettingPrefs.getBoolean("NightMode", false)
+        val button1: Button = findViewById(R.id.button1)
+
+        if(isNightModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            button1.text = changeButtonMessage()
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            button1.text = changeButtonMessage()
+        }
+
+        button1.setOnClickListener(View.OnClickListener {
+            if(isNightModeOn){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPrefsEdit.putBoolean("NightMode", false)
+                sharedPrefsEdit.apply()
+
+                button1.text = changeButtonMessage()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPrefsEdit.putBoolean("NightMode", true)
+                sharedPrefsEdit.apply()
+
+                button1.text = changeButtonMessage()
+            }
+        })
+        changeThemeMessage()
+
+    }
+
+    fun changeThemeMessage():String{
+        return "change theme"
+    }
+
+    fun changeButtonMessage(): String {
+        if (isNightModeOn) {
+            return "Light Mode"
+        }
+        return "Dark Mode"
     }
 
 }
